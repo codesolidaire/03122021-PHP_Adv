08c990cb5e7981e8f3c1a57cf0270150a7749b36
Bobacar BOUTAYEB <codesolidaire@gmail.com>
2021-12-06 19:00:04 +0100

    add all

---
+++ connec.php
@@ -0,0 +1,4 @@
+<?php
+define('DSN', 'mysql:host=localhost;dbname=database_name');
+define('USER', 'root');
+define('PASS', '');
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once '_connec.php';
+
+$pdo = new \PDO(DSN, USER, PASS);
